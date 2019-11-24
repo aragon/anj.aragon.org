ca61fdff8a1193bda839f2f3d8a526cd37cc3cb6
delfipolito <delfinapolito@gmail.com>
2019-11-24 16:11:34 -0300

    change on react-static config file

--- static.config.js
+++ static.config.js
@@ -1,5 +1,15 @@
 import path from 'path'
 import React from 'react'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+const REACT_STATIC_PATHS = {
+  src: 'src',
+  dist: 'dist',
+  devDist: 'dist',
+  public: 'public',
+}
+const ARAGON_UI_ASSETS = path.dirname(require.resolve('@aragon/ui'))
 
 export default {
   siteRoot: process.env.SITE_ROOT || '',
@@ -11,6 +21,7 @@ export default {
       getData: () => ({ title: '' }),
     },
   ],
+  paths: REACT_STATIC_PATHS,
   plugins: [
     [
       require.resolve('react-static-plugin-source-filesystem'),
@@ -21,6 +32,46 @@ export default {
     require.resolve('react-static-plugin-reach-router'),
     require.resolve('react-static-plugin-sitemap'),
   ],
+  webpack: (conf, { defaultLoaders }) => {
+    conf.resolve = Object.assign({}, conf.resolve || {}, {
+      // Needed when @aragon/ui is linked (development)
+      modules: ((conf.resolve && conf.resolve.modules) || []).concat([
+        path.join(__dirname, 'node_modules'),
+        path.join(__dirname, REACT_STATIC_PATHS.dist),
+      ]),
+    })
+
+    // Copy the Aragon UI assets to dist/aragon-ui. CopyWebpackPlugin is aware
+    // of webpack-dev-server so there is no need to add this directory to the
+    // devServer configuration.
+    conf.plugins = (conf.plugins || []).concat([
+      new CopyWebpackPlugin([
+        {
+          from: ARAGON_UI_ASSETS,
+          to: path.resolve(
+            path.join(__dirname, REACT_STATIC_PATHS.dist, 'aragon-ui')
+          ),
+        },
+      ]),
+    ])
+
+    const fileLoader = defaultLoaders.fileLoader
+    fileLoader.query.name = 'static/[hash:8]-[name].[ext]'
+    conf.module.rules = [
+      {
+        oneOf: [defaultLoaders.jsLoader, defaultLoaders.cssLoader, fileLoader],
+      },
+    ]
+    conf.plugins.push(new ExtractTextPlugin('app.css'))
+
+    return conf
+  },
+  renderToHtml: (render, Comp, meta) => {
+    const sheet = new ServerStyleSheet()
+    const html = render(sheet.collectStyles(<Comp />))
+    meta.styleTags = sheet.getStyleElement()
+    return html
+  },
   Document: class CustomHtml extends React.Component {
     render() {
       const {
