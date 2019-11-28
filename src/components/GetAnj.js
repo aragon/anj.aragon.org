269a32726ec9f74682ecd03d57836ea4edda7a08
delfipolito <delfinapolito@gmail.com>
2019-11-28 17:10:21 -0300

    post link

--- src/components/GetAnj.js
+++ src/components/GetAnj.js
@@ -13,7 +13,7 @@ const GetAnj = () => (
   <GetAnjSection id="get-anj">
     <Content className="medium" src={content} />
     <Content className="mobile" src={contentMobile} />
-    <a href="https://blog.aragon.org/announcing-aragon-ui-1-0/" target="_blank">
+    <a href="https://blog.aragon.org/aragon-court/" target="_blank">
       <img className="medium" src={comingSoon} />
       <img className="mobile" src={comingSoonMobile} />
     </a>
