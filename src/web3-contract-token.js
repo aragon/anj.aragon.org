3442e90fd1cc37c054e322e2cf1868b4cce95a56
Pierre Bertet <hello@pierre.world>
2019-12-17 17:44:47 -0300

    web3-contract-token: add useTokenDecimals() + use new hooks

--- src/web3-contract-token.js
+++ src/web3-contract-token.js
@@ -1,46 +1,59 @@
 import { useEffect, useState, useCallback } from 'react'
 import { Contract as EthersContract } from '@ethersproject/contracts'
 import { BigNumber } from '@ethersproject/bignumber'
-import { useWeb3Connect } from './web3-connect'
+import { getKnownContract } from './known-contracts'
 import { balanceFromBigInt } from './utils'
-import KNOWN_CONTRACTS_ALL from './known-contracts'
-import env from './environment'
-import balanceOfAbi from './abi/token-balanceof.json'
-import jurorsRegistryAbi from './abi/jurors-registry.json'
+import { useWeb3Connect } from './web3-connect'
+import { useKnownContract } from './web3-contract'
+
 import approveAndCallAbi from './abi/wrapper.json'
 
-const KNOWN_CONTRACTS = KNOWN_CONTRACTS_ALL.get(env('CHAIN_ID')) || {}
+export function useTokenDecimals(symbol) {
+  const [decimals, setDecimals] = useState(-1)
+  const tokenContract = useKnownContract(`TOKEN_${symbol}`)
+
+  useEffect(() => {
+    let cancelled = false
+    setDecimals(-1)
+
+    if (tokenContract) {
+      tokenContract.decimals().then(decimals => {
+        if (!cancelled) {
+          setDecimals(decimals)
+        }
+      })
+    }
+
+    return () => (cancelled = true)
+  }, [tokenContract])
+
+  return decimals
+}
 
 export function useTokenBalance(symbol) {
   const { account, ethersProvider } = useWeb3Connect()
   const [balance, setBalance] = useState(balanceFromBigInt(-1))
+  const tokenContract = useKnownContract(`TOKEN_${symbol}`)
+  const decimals = useTokenDecimals(symbol)
 
-  const contractAddress = KNOWN_CONTRACTS[`TOKEN_${symbol}`]
+  useEffect(() => {
+    let cancelled = false
 
-  useEffect(
-    () => {
-      if (!account || !ethersProvider || !contractAddress) {
-        setBalance(balanceFromBigInt(-1))
-        return
-      }
+    if (!account || !tokenContract) {
+      setBalance(balanceFromBigInt(-1))
+      return
+    }
 
-      const tokenContract = new EthersContract(
-        contractAddress,
-        balanceOfAbi,
-        ethersProvider
-      )
-
-      Promise.all([
-        tokenContract.balanceOf(account),
-        tokenContract.decimals(),
-      ]).then(([balance, decimals]) => {
+    tokenContract.balanceOf(account).then(balance => {
+      if (!cancelled) {
         setBalance(
           balanceFromBigInt(balance.div(BigNumber.from(10).pow(decimals)))
         )
-      })
-    },
-    [account, ethersProvider, contractAddress]
-  )
+      }
+    })
+
+    return () => (cancelled = true)
+  }, [account, tokenContract, decimals])
 
   return balance
 }
@@ -49,109 +62,45 @@ export function useJurorRegistryAnjBalance() {
   const { account, ethersProvider } = useWeb3Connect()
   const [balance, setBalance] = useState(balanceFromBigInt(-1))
 
-  const tokenAddress = KNOWN_CONTRACTS['TOKEN_ANJ']
-  const jurorsRegistryAddress = KNOWN_CONTRACTS['JURORS_REGISTRY']
+  const tokenContract = useKnownContract('TOKEN_ANJ')
+  const jurorsRegistryContract = useKnownContract('JURORS_REGISTRY')
 
-  useEffect(
-    () => {
-      if (!account || !ethersProvider || !tokenAddress) {
-        setBalance(balanceFromBigInt(-1))
-        return
-      }
+  useEffect(() => {
+    if (!account || !tokenContract || !jurorsRegistryContract) {
+      setBalance(balanceFromBigInt(-1))
+      return
+    }
 
-      const tokenContract = new EthersContract(
-        tokenAddress,
-        balanceOfAbi,
-        ethersProvider
+    Promise.all([
+      jurorsRegistryContract.balanceOf(account),
+      tokenContract.decimals(),
+    ]).then(([balances, decimals]) => {
+      setBalance(
+        balanceFromBigInt(balances[0].div(BigNumber.from(10).pow(decimals)))
       )
-
-      const jurorsRegistryContract = new EthersContract(
-        jurorsRegistryAddress,
-        jurorsRegistryAbi,
-        ethersProvider
-      )
-
-      Promise.all([
-        jurorsRegistryContract.balanceOf(account),
-        tokenContract.decimals(),
-      ]).then(([balances, decimals]) => {
-        setBalance(
-          balanceFromBigInt(balances[0].div(BigNumber.from(10).pow(decimals)))
-        )
-      })
-    },
-    [account, ethersProvider, tokenAddress]
-  )
+    })
+  }, [account, tokenContract, jurorsRegistryContract])
 
   return balance
 }
 
 // Convert ANT to ANJ action
-export function useConvertAntToAnj(onDone = done) {
+export function useConvertAntToAnj() {
   const { ethersProvider } = useWeb3Connect()
+
+  const antContract = useKnownContract('TOKEN_ANT')
+  const [wrapperAddress] = getKnownContract('WRAPPER')
+
   return useCallback(
     async amount => {
-      if (!ethersProvider) {
-        return
+      if (!antContract || !wrapperAddress) {
+        return false
       }
-      const antAddress = KNOWN_CONTRACTS['TOKEN_ANT']
-      const wrapperAddress = KNOWN_CONTRACTS['WRAPPER']
-      const antContract = new EthersContract(
-        antAddress,
-        approveAndCallAbi,
-        ethersProvider.getSigner()
-      )
-      let overrides = {
+
+      return antContract.approveAndCall(wrapperAddress, amount, '0x00', {
         gasLimit: 7500000,
-      }
-      antContract
-        .approveAndCall(wrapperAddress, amount, '0x00', overrides)
-        .then(onDone)
+      })
     },
-    [onDone, ethersProvider]
+    [antContract, wrapperAddress]
   )
-}
-
-function done() {
-  console.log('done')
-}
-
-export function useConvertLogic() {
-  const actions = {
-    convertAntToAnj: useConvertAntToAnj(),
-  }
-
-  return {
-    actions,
-  }
-}
-
-export function useTokenToUsd(token, balance) {
-  const [usd, setUsd] = useState('-')
-  useEffect(
-    () => {
-      fetch(
-        'https://min-api.cryptocompare.com/data/price?fsym=' +
-          token +
-          '&tsyms=USD'
-      )
-        .then(response => {
-          return response.json()
-        })
-        .then(price => {
-          if (parseFloat(balance) > 0) {
-            https: setUsd(
-              balanceFromBigInt(
-                balance.value
-                  .mul(BigNumber.from(parseInt(price.USD * 1000000, 10)))
-                  .div(1000000)
-              ).toString()
-            )
-          }
-        })
-    },
-    [balance]
-  )
-
-  return usd
 }
