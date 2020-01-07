ee8d8dea07b29bb707d818ceeb26a7968ff80136
Pierre Bertet <hello@pierre.world>
2020-01-07 19:03:13 +0100

    Enable converter (#14)

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,15 @@ import BecomeAJuror from '../components/BecomeAJuror'
 import Converter from '../components/converter/Converter'
 import Features from '../components/Features'
 import Footer from '../components/Footer'
-import GetAnj from '../components/GetAnj'
 import Hero from '../components/Hero'
 import Subscribe from '../components/Subscribe'
 import WhatHappens from '../components/WhatHappens'
-import { ENABLE_CONVERTER_AT } from '../utils'
 
 export default () => {
-  const now = new Date()
   return (
     <div>
       <Hero />
-      {now > ENABLE_CONVERTER_AT ? <Converter /> : <GetAnj />}
+      <Converter />
       <Features />
       <WhatHappens />
       <About />
